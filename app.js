aaddec9087c880727ffa140e9b4315a605fbdadf
hei <justinckhcheung@hotmail.com>
2024-05-31 17:43:19 +0800

    debug

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 document.getElementById("trackButton").addEventListener("click", function () {
+  console.log("Button clicked! Sending event to GA4...");
   gtag("event", "button_click", {
     event_category: "Button",
     event_label: "Track Event Button Clicked",
-    button_name: "Track Event",
-    interaction_type: "Click",
   });
 });
