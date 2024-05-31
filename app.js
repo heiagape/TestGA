1020c9e7c456a3fc83b24bf1e727439965943686
hei <justinckhcheung@hotmail.com>
2024-05-31 17:32:01 +0800

    GA

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ document.getElementById("trackButton").addEventListener("click", function () {
   gtag("event", "button_click", {
     event_category: "Button",
     event_label: "Track Event Button Clicked",
+    button_name: "Track Event",
+    interaction_type: "Click",
   });
 });
